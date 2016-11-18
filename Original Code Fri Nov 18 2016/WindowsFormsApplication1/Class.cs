a43fca7cb9b75aec61a24d3955c0092366b5c4b2
MUSAWER MOBARAK <1MOBARAKMUS@hdsb.ca>
2016-11-18 12:59:02 -0500

    Original code Nov 18

---
+++ Original Code Fri Nov 18 2016/WindowsFormsApplication1/Class.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication1
+{
+    class Class
+    {
+        public Class(string className)
+        {
+            //Class has:
+                //Students
+                //Teachers
+                //Name
+                //Size
+
+            string name = className;
+            int size;
+        }
+    }
+}
